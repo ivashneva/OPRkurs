0467c7b781d9ea23b23e6f466a685ee9329d8947
ivashneva <evialina@gmail.com>
2021-12-03 21:41:39 +0300

    reading from a file 12345

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,22 +9,22 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //123
-        System.out.print("Test123");
-        try(FileWriter writer = new FileWriter("notes3.txt", false))
-        {
-            // запись всей строки
-            String text = "Evelinka";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-
-            writer.flush();
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
+       // System.out.print("Test123");
+//        try(FileWriter writer = new FileWriter("notes3.txt", false))
+//        {
+//            // запись всей строки
+//            String text = "Evelinka";
+//            writer.write(text);
+//            // запись по символам
+//            writer.append('\n');
+//            writer.append('E');
+//
+//            writer.flush();
+//        }
+//        catch(IOException ex){
+//
+//            System.out.println(ex.getMessage());
+//        }
         try(FileReader reader = new FileReader("notes3.txt"))
         {
             // читаем посимвольно
