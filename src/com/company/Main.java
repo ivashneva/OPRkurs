27f1af2a6d1c6faa9432413496c41313b6b1003e
ivashneva <evialina@gmail.com>
2021-12-03 21:31:12 +0300

    reading\writing from a file

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //123
+        System.out.print("Test123");
+        try(FileWriter writer = new FileWriter("notes3.txt", false))
+        {
+            // запись всей строки
+            String text = "Evelinka";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+        try(FileReader reader = new FileReader("notes3.txt"))
+        {
+            // читаем посимвольно
+            int c;
+            while((c=reader.read())!=-1){
+
+                System.out.print((char)c);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
